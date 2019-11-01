5037ba006ae3564735da3f7fc1253cf1a46c08d0
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-11-01 23:22:34 +0100

    [ADD] Add assembling files for release to create.sh

--- create.sh
+++ create.sh
@@ -11,3 +11,16 @@ make epub
 make man
 make text
 make gettext
+
+mkdir build/release
+cd build
+zip -r release/html html
+zip -r release/gettext gettext
+zip -r release/htmlhelp htmlhelp
+zip -r release/text text
+zip -r release/singlehtml singlehtml
+zip -r release/dirhtml dirhtml
+cp man/thebookofv.1 release/
+cp devhelp/TheBookofV.devhelp.gz release/
+cp epub/TheBookofV.epub release/
+cd ..
